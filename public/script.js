cbbdb5c08c76a226c30d742897c7ad738530a32c
Ananth Rao <ananth3rao24@gmail.com>
2021-03-30 15:17:13 -0400

    Adding Colors

--- public/script.js
+++ public/script.js
@@ -1,21 +1,22 @@
 async function getDiningHalls() {
-    const response = await fetch("/api/dining");
-    const data = await response.json();
-    diningHalls = data.data;
-    console.log(diningHalls);
-    const halls = document.querySelector(".halls")
-    const html = diningHalls.map((hall) =>
-    `<tr>
-        <th> ${hall.hall_name} </th>
+  const response = await fetch("/api/dining");
+  const data = await response.json();
+  diningHalls = data.data;
+  console.log(diningHalls);
+  const halls = document.querySelector(".halls");
+  const html = diningHalls
+    .map(
+      (hall) =>
+        `<tr>
+        <td> ${hall.hall_name} </th>
         <td> ${hall.hall_address}</td>
     </tr>`
-    ).join("");
-    halls.innerHTML = html;
-
-
+    )
+    .join("");
+  halls.innerHTML = html;
 }
 async function windowActions() {
-    getDiningHalls();
-  }
-  
-  window.onload = windowActions;
+  getDiningHalls();
+}
+
+window.onload = windowActions;
